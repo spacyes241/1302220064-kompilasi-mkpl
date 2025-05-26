c7fbd04677c14f8fa2f3886abe96c9023ba02228
Spacyes241 <farrelhaykalindonesia@gmail.com>
2025-05-26 13:45:40 +0700

    bla

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -16,17 +16,7 @@ public class Driver {
 		
 		System.out.println("Current Count:" + counter.getCount());
 		
-		counter.increaseBy(5);
-
-		System.out.println("Current count: " + counter.getCount());
-
-		counter.decreaseBy(2);
-
-		System.out.println("Current count: " + counter.getCount());
-
-		counter.multiplyBy(6);
-
-		System.out.println("Current count: " + counter.getCount());
+		
 
 	}
 
