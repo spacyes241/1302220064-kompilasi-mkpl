31c4548682e7227c544e318bfcc87cf3ce1df32c
Spacyes241 <farrelhaykalindonesia@gmail.com>
2025-06-02 18:18:04 +0700

    test
    zcac

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -14,6 +14,20 @@ public class Driver {
 		
 		counter.decrement();
 		
+		System.out.println("Current Count:" + counter.getCount());
+
+		counter.increaseBy(5);
+
+		System.out.println("Current count: " + counter.getCount());
+
+		counter.decreaseBy(2);
+
+		System.out.println("Current count: " + counter.getCount());
+
+		counter.multiplyBy(6);
+
+		System.out.println("Current count: " + counter.getCount());
+
 		
 		
 
