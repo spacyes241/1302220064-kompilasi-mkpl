16660d5c32480c1fff9c0e8287a25bd2258aff18
Spacyes241 <farrelhaykalindonesia@gmail.com>
2025-06-02 18:14:58 +0700

    test

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -11,17 +11,4 @@ public class Counter {
 	public void reset() {
 		count = 0;
 	}
-	
-	public void increment() {
-		count++;
-	}
-	
-	public void decrement() {
-		count--;
-	}
-	
-	public int getCount() {
-		return count;
-	}
-	
 }
