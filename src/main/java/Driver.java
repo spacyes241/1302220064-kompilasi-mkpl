94e5d2c93f86b40e7f0ec2464e818efcdda1ce04
Spacyes241 <farrelhaykalindonesia@gmail.com>
2025-06-02 18:23:11 +0700

    co0mmit

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -6,31 +6,16 @@ public class Driver {
 		
 		Counter counter = new Counter();
 		
-		System.out.println("Current Count:" + counter.getCount());
+		System.out.println("Current count:" + counter.getCount());
 		
 		counter.increment();
 		
-		System.out.println("Current Count:" + counter.getCount());
+		System.out.println("Current count:" + counter.getCount());
 		
 		counter.decrement();
 		
-		System.out.println("Current Count:" + counter.getCount());
-
-		counter.increaseBy(5);
-
-		System.out.println("Current count: " + counter.getCount());
-
-		counter.decreaseBy(2);
-
-		System.out.println("Current count: " + counter.getCount());
-
-		counter.multiplyBy(6);
-
-		System.out.println("Current count: " + counter.getCount());
-
+		System.out.println("Current count:" + counter.getCount());
 		
-		
-
 	}
 
 }
